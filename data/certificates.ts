26e7b635e7feeba06bcaa26d52700cf94a1519b3
Ray <cancinoray@gmail.com>
2026-01-22 11:16:12 +0800

    add cert for data science

--- data/certificates.ts
+++ data/certificates.ts
@@ -8,5 +8,12 @@ export const certificates: Certificate[] = [
     credentialUrl: "https://certificate.datatalks.club/dezoomcamp/2025/e09e31eb7761ba2e72355039a36025eec730df12.pdf",
     image: "/assets/certificates/1.png"
   },
+  {
+    title: "Complete A.I. & Machine Learning, Data Science Bootcamp",
+    issuer: "DataTalks.Club",
+    date: "2024",
+    credentialUrl: "https://www.udemy.com/certificate/UC-a441674f-00a1-484a-a0a6-907c36c71c0e/",
+    image: "/assets/certificates/2.png"
+  },
 ];
 
